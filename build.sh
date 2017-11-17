15d34316aabb03626dc2fadb69fd646e0e516730
Jon Carlson <joncrlsn@gmail.com>
2017-11-17 17:42:53 -0600

    Refactored to remove bin directories

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #
-# For OSX and Linux, this script:
+# Builds executable and downloadable bundle for 3 platforms
+#
+# For OSX and Linux:
 #  * builds pgdiff 
 #  * downloads pgrun 
-#  * combines them and pgdiff.sh into a tgz file
+#  * combines them, a README, and pgdiff.sh into a tgz file
+#
+# For Windows:
+#  * builds pgdiff.exe
+#  * downloads pgrun.exe
+#  * combines them, a README, and pgdiff.sh into a zip file
 #
 
 SCRIPT_DIR="$(dirname `ls -l $0 | awk '{ print $NF }'`)"
@@ -11,7 +18,16 @@ SCRIPT_DIR="$(dirname `ls -l $0 | awk '{ print $NF }'`)"
 [[ -z $APPNAME ]] && APPNAME=pgdiff
 [[ -z $VERSION ]] && read -p "Enter version number: " VERSION
 
-if [[ -d bin-linux ]]; then
+LINUX_README=README-linux.md
+LINUX_FILE="${APPNAME}-linux-${VERSION}.tar.gz"
+
+OSX_README=README-osx.md
+OSX_FILE="${APPNAME}-osx-${VERSION}.tar.gz"
+
+WIN_README=README-win.md
+WIN_FILE="${APPNAME}-win-${VERSION}.zip"
+
+if [[ -f $LINUX_README ]]; then
     echo "  ==== Building Linux ===="
     tempdir="$(mktemp -d)"
     workdir="$tempdir/$APPNAME"
@@ -23,20 +39,20 @@ if [[ -d bin-linux ]]; then
     wget -O "$workdir/pgrun" "https://github.com/joncrlsn/pgrun/raw/master/bin-linux/pgrun"
     # Copy the bash runtime script to the temp directory
     cp pgdiff.sh "$workdir/"
-    cp "${SCRIPT_DIR}/bin-linux/README.md" "$workdir/"
+    cp "${SCRIPT_DIR}/${LINUX_README}" "$workdir/README.md"
     cd "$tempdir"
     # Make everything executable
     chmod -v ugo+x $APPNAME/*
-    tarName="${tempdir}/${APPNAME}-linux-${VERSION}.tar.gz"
+    tarName="${tempdir}/${LINUX_FILE}"
     COPYFILE_DISABLE=true tar -cvzf "$tarName" $APPNAME
     cd -
-    mv "$tarName" "${SCRIPT_DIR}/bin-linux/"
+    mv "$tarName" "${SCRIPT_DIR}/"
     echo "Built linux."
 else
-    echo "Skipping linux.  No bin-linux directory."
+    echo "Skipping linux.  No $LINUX_README file."
 fi
 
-if [[ -d bin-osx ]]; then
+if [[ -f $OSX_README ]]; then
     echo "  ==== Building OSX ===="
     tempdir="$(mktemp -d)"
     workdir="$tempdir/$APPNAME"
@@ -48,20 +64,20 @@ if [[ -d bin-osx ]]; then
     wget -O "$workdir/pgrun" "https://github.com/joncrlsn/pgrun/raw/master/bin-osx/pgrun"
     # Copy the bash runtime script to the temp directory
     cp pgdiff.sh "$workdir/"
-    cp "${SCRIPT_DIR}/bin-osx/README.md" "$workdir/"
+    cp "${SCRIPT_DIR}/${OSX_README}" "$workdir/README.md"
     cd "$tempdir"
     # Make everything executable
     chmod -v ugo+x $APPNAME/*
-    tarName="${tempdir}/${APPNAME}-osx-${VERSION}.tar.gz"
+    tarName="${tempdir}/${OSX_FILE}"
     COPYFILE_DISABLE=true tar -cvzf "$tarName" $APPNAME
     cd -
-    mv "$tarName" "${SCRIPT_DIR}/bin-osx/"
+    mv "$tarName" "${SCRIPT_DIR}/"
     echo "Built osx."
 else
-    echo "Skipping osx.  No bin-osx directory."
+    echo "Skipping osx.  No $OSX_README file."
 fi
 
-if [[ -d bin-win ]]; then
+if [[ -f $WIN_README ]]; then
     echo "  ==== Building Windows ===="
     tempdir="$(mktemp -d)"
     workdir="$tempdir/$APPNAME"
@@ -70,16 +86,16 @@ if [[ -d bin-win ]]; then
     GOOS=windows GOARCH=386 go build -o "${workdir}/${APPNAME}.exe"
     # Download pgrun to the work directory 
     # Copy the bash runtime script to the temp directory
-    cp "${SCRIPT_DIR}/bin-win/README.md" "$workdir/"
+    cp "${SCRIPT_DIR}/${WIN_README}" "$workdir/README.md"
     cd "$tempdir"
     # Make everything executable
     chmod -v ugo+x $APPNAME/*
     wget -O "${workdir}/pgrun.exe" "https://github.com/joncrlsn/pgrun/raw/master/bin-win/pgrun.exe"
-    zipName="${tempdir}/${APPNAME}-win-${VERSION}.zip"
+    zipName="${tempdir}/${WIN_FILE}"
     zip -r "$zipName" $APPNAME
     cd -
-    mv "$zipName" "${SCRIPT_DIR}/bin-win/"
+    mv "$zipName" "${SCRIPT_DIR}/"
     echo "Built win."
 else
-    echo "Skipping win.  No bin-win directory."
+    echo "Skipping win.  No $WIN_README file."
 fi
